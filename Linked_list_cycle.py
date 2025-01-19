e404bfedac65801729eeae972ef5bd9609d836db
Jayganeshcj <40379698+Jayganeshcj@users.noreply.github.com>
2025-01-20 01:39:15 +0530

    Update Linked_list_cycle.py

--- Linked_list_cycle.py
+++ Linked_list_cycle.py
@@ -1,1 +1,18 @@
-print("Hello World")
+# Definition of singly-linked list.
+# class ListNode:
+#   def __init__(self, x):
+#     self.val = x
+#     self.next = None
+
+class Solution:
+  def hasCycle(self, head: Optional[ListNode]) -> bool:
+    hare = tortise = head
+    while hare and hare.next:
+      hare = hare.next.next
+      tortise = tortise.next
+      if tortise == hare:
+        return True
+
+    return False
+    
+    
